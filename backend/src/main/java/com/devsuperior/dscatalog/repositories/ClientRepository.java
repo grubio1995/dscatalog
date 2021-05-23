e83cff7413dd2b70079d7d8e9a76af5eb9723b0a
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-05-23 19:19:44 -0300

    Created Client entity,repository and DTO

---
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ClientRepository.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ch.qos.logback.core.net.server.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+	
+	
+}
