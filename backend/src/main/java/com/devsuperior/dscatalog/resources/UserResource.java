0eb6614720a9cff26534a6e136e206af413c05a6
Gabriel Rubio <gabrielpinhatarubio@gmail.com>
2021-06-02 22:47:45 -0300

    Basic beans validation

--- backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
@@ -3,6 +3,8 @@ package com.devsuperior.dscatalog.resources;
 
 import java.net.URI;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -41,14 +43,14 @@ public class UserResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<UserDTO> insert(@RequestBody UserInsertDTO dto) {
+	public ResponseEntity<UserDTO> insert(@Valid @RequestBody UserInsertDTO dto) {
 		UserDTO userDto = service.insert(dto);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userDto.getId()).toUri();
 		return ResponseEntity.created(uri).body(userDto);
 	}
 	
 	@PutMapping(value="/{id}")
-	public ResponseEntity<UserDTO> update(@PathVariable Long id, @RequestBody UserDTO dto) {
+	public ResponseEntity<UserDTO> update(@Valid @PathVariable Long id, @RequestBody UserDTO dto) {
 		dto = service.update(id,dto);
 		return ResponseEntity.ok().body(dto);
 	}
